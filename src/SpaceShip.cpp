78241df034b1a53b93e8c73f31cf601574868b86
Sean <sean.jasmins-nelson@georgebrown.ca>
2021-01-24 20:14:20 -0500

    Finished seek to target and changed spaceship.png

--- src/SpaceShip.cpp
+++ src/SpaceShip.cpp
@@ -1,5 +1,7 @@
 #include "SpaceShip.h""
 
+#include "Util.h"
+
 SpaceShip::SpaceShip()
 {
 	TextureManager::Instance()->load("../Assets/textures/spaceship.png", "spaceship");
@@ -13,6 +15,9 @@ SpaceShip::SpaceShip()
 	getRigidBody()->acceleration = glm::vec2(0.0f, 0.0f);
 	getRigidBody()->isColliding = false;
 	setType(SPACE_SHIP);
+	setMaxSpeed(10.0f);
+	setOrientation(glm::vec2(0.0f, -1.0f));
+	setRotation(0.0f);
 }
 
 SpaceShip::~SpaceShip()
@@ -20,14 +25,55 @@ SpaceShip::~SpaceShip()
 
 void SpaceShip::draw()
 {
-	TextureManager::Instance()->draw("spaceship", getTransform()->position.x, getTransform()->position.y,
-		0, 255, true);
+	TextureManager::Instance()->draw("spaceship",
+		getTransform()->position.x, getTransform()->position.y, m_rotationAngle, 255, true);
+
+	Util::DrawLine(getTransform()->position, getTransform()->position + m_orientation * 60.0f);
 }
 
 void SpaceShip::update()
 {
+	m_Move();
 }
 
 void SpaceShip::clean()
 {
+}
+
+void SpaceShip::setDestination(const glm::vec2 destination)
+{
+	m_destination = destination;
+}
+
+void SpaceShip::setMaxSpeed(const float speed)
+{
+	m_maxSpeed = speed;
+}
+
+void SpaceShip::setOrientation(glm::vec2 orientation)
+{
+	m_orientation = orientation;
+}
+
+void SpaceShip::setRotation(float angle)
+{
+	m_rotationAngle = angle;
+}
+
+float SpaceShip::getRotation() const
+{
+	return m_rotationAngle;
+}
+
+void SpaceShip::m_Move()
+{
+	// direction with magnitude
+	m_targetDirection = m_destination - getTransform()->position;
+
+	// normalized direction
+	m_targetDirection = Util::normalize(m_targetDirection);
+
+	getRigidBody()->velocity = m_targetDirection * m_maxSpeed;
+
+	getTransform()->position += getRigidBody()->velocity;
 }
--- src/SpaceShip.h
+++ src/SpaceShip.h
@@ -18,9 +18,23 @@ public:
 	void update() override;
 	void clean() override;
 
+	// getters and setters
+	void setDestination(const glm::vec2 destination);
+	void setMaxSpeed(const float speed);
+	void setOrientation(glm::vec2 orientation);
+	void setRotation(float angle);
+	float getRotation() const;
+
 private:
 
-	
+	glm::vec2 m_destination;
+	glm::vec2 m_targetDirection;
+	glm::vec2 m_orientation;
+	float m_rotationAngle;
+	float m_maxSpeed;
+
+	// private function
+	void m_Move();
 };
 
 #endif // defined (__SPACE_SHIP__)
